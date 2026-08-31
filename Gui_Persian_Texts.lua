ebd909b33601288fa387dc736fdd815085671747
Shomal <HiradShomboli2000@gmail.com>
2026-08-31 21:46:30 +0330

    Add files via upload

---
+++ Gui_Persian_Texts.lua
@@ -0,0 +1,137 @@
+return {
+    Tabs = {
+        "تنظیمات منو",
+        "شخصیت",
+        "خودرو",
+        "هدف گيري",
+        "چيت هاي ويژه",
+        "(ESP) وال هک",
+        "اطلاعات سازنده"
+    },
+    GuiTexts = {
+        MainPanelTitle = "پنل هنر تقلب )نسخه بتا و آزمايشي و فارسي سازي شده(",
+        OwnerTitle = "مديرکل مجموعه هنر تفلب:",
+        DeveloperTitle = "طراح و نويسنده:",
+        TelegramTitle = "کانال تلگرام:",
+        YoutubeTitle = "کانال یوتیوب:",
+        
+        -- Tab 1
+        KeySettingsHeader = "تنظیمات کلید منو و میانبرها",
+        OpenCloseKeySetting = "تنظیم کلید باز و بسته کردن منو:",
+        PressAnyKey = "یک کلید را فشار دهید...",
+        KeyPrefix = "کليد ",
+        KeyChangeHint = "براي تغيير کليد روي دکمه بالا کليک کنيد",
+        
+        -- Tab 2
+        IncreaseHealth = "افزایش سلامتی",
+        IncreaseArmor = "افزایش زره",
+        Suicide = "خودکشی",
+        ReSpawn = "ري اسپاون",
+        Teleport = "تلپورت",
+        TeleportError = "{FF0000}[ خطا! ] {FFFFFF}نقطه مشخص‌شده پیدا نشد؛ {FFFFFF}لطفاً مکان موردنظر را روی نقشه علامت بزنید.",
+        Unfreeze = "رفع انجماد",
+        WeaponHack = "هک اسلحه",
+        FakeSkin = "اسکین جعلی",
+        GetJetpack = "دريافت جت پک",
+        GiveWanted = "وانتد دادن",
+        SpeedRun = "افزایش سرعت دویدن",
+        FlyHack = "پرواز",
+        FlyEnabled = "{111111}[ {FFFFFF}@{FF00FF}Art{00FFFF}Of{FFFF00}Cheat {111111}] {00FF00}[پرواز] {FFFFFF}فعال شد. {00FFFF}سرعت فعلی: {FFFFFF}",
+        FlyDisabled = "{111111}[ {FFFFFF}@{FF00FF}Art{00FFFF}Of{FFFF00}Cheat {111111}] {FF0000}[پرواز] {FFFFFF}غیرفعال شد.",
+        GodMode = "جان بي نهايت",
+        Attention = "توجه",
+        InfinityOxygen = "نفس بي نهايت",
+        InfinityRun = "دويدن بي نهايت",
+        Invisible = "نامرئی",
+        AutoCBug = "سي باگ خودکار",
+        RapidFire = "شلیک سریع",
+        AntiGreenZone = "شليک در گرين زون",
+        AirBrake = "اير بريک",
+        TrollFoot = "پرتاب بازيکن ها در حالتي که پياده هستيد",
+        TrollOnMsg = "چيت روشن شد نزديک بازيکن هاي مد نظر شيد",
+        
+        -- Tab 3
+        RepairCar = "تعمیر خودرو",
+        Nitro = "نیتروژن",
+        UnfreezeCar = "رفع انجماد خودرو",
+        FlipCar = "صاف کردن خودرو",
+        SpawnCar = "اسپاون ماشين",
+        EjectCar = "اجکت ماشين",
+        CarGodMode = "گاد مود",
+        AutoFlip = "صاف کردن خودکار",
+        CarSpeedHack = "افزایش سرعت خودرو",
+        CarSpeedOnMsg = "{111111}[ {FFFFFF}@{FF00FF}Art{00FFFF}Of{FFFF00}Cheat {111111}] {FFFFFF}افزایش سرعت {00FF00}فعال شد. {FFFFFF}فشار دهید: {999999}W یا S",
+        CarSpeedOffMsg = "{111111}[ {FFFFFF}@{FF00FF}Art{00FFFF}Of{FFFF00}Cheat {111111}] {FFFFFF}افزایش سرعت {FF0000}غیرفعال شد.",
+        CarFlyHack = "پرواز",
+        CarFlyOnMsg = "{111111}[ {FFFFFF}@{FF00FF}Art{00FFFF}Of{FFFF00}Cheat {111111}] {00FF00}[پرواز خودرو] فعال شد! {FFFFFF}برای حرکت از W و برای تغییر جهت از دوربین استفاده کنید.",
+        CarFlyOffMsg = "{111111}[ {FFFFFF}@{FF00FF}Art{00FFFF}Of{FFFF00}Cheat {111111}] {FF0000}[پرواز خودرو] غیرفعال شد!",
+        AntiBikeFall = "عدم سقوط از موتور موقع تصادف",
+        InfinityFuel = "بنزين نا محدود و روشن ماندن خودرو",
+        DriveAllCars = "سوار شدن به همه ماشين هاي فکشن",
+        TrollCar = "پرتاب بازيکن ها وقتي سوار ماشين هستيد",
+        
+        -- Tab 4
+        LockAimBot = "قفل هدف گيري )هنگام شليک(",
+        SilentAimBot = "سايلنت ايم بات",
+        
+        -- Tab 5
+        Settings = "تنظیمات",
+        SpecNotification = "اعلان تماشاگر )اسپکت فايندر(",
+        OnlineNotification = "ديدن ليست ادمين هاي آنلاين",
+        TeleportJobBot = "جاب بات مخصوص شعل هايي",
+        CheckPointHint = "که چک پوينت قرمز دارن مثل چوب بري",
+        PrisonBreak = "خروج و فرار از زندان",
+        FishingBot = "ربات ماهیگیری",
+        RpGodMode = "Rp گاد مود مخصوص سرور هاي",
+        
+        -- Tab 6
+        TwoDBoxESP = "جعبه دو بعدي",
+        ThreeDBoxESP = "جعبه سه بعدي",
+        LineESP = "وال هک خطي",
+        AimingESP = "محل شليک",
+        ObjectFinderID = "آبجکت فايندر با آيدي",
+        
+        -- Sub Windows
+        WeaponHackTitle = "هک اسلحه",
+        WeaponIDInput = "شناسه اسلحه",
+        BulletAmountInput = "تعداد گلوله",
+        SubmitWeapon = "ثبت اسلحه",
+        CloseBtn = "بستن",
+        
+        FakeSkinTitle = "اسکین جعلی",
+        SkinIDInput = "شناسه اسکین",
+        SubmitSkin = "ثبت اسکین",
+        
+        GiveWantedTitle = "دادن وانتد به بازيکنا )فيک کيل(",
+        PlayerIDInput = "آيدي بازيکن",
+        FakeKillBtn = "فيک کيل",
+        
+        SpeedRunTitle = "افزایش سرعت دویدن",
+        SpeedAmount = "مقدار سرعت",
+        
+        SpawnCarTitle = "اسپاون ماشين",
+        CarIDInput = "آيدي ماشين",
+        SpawnCarBtn = "اسپاون ماشين",
+        
+        EjectCarTitle = "تلپورت و سوار شدن به ماشين",
+        CommandLabel = ": کامند",
+        CommandDL = "/DL",
+        CommandHint = "بزنيد تا آيدي ماشين هارو با رنگ آبي ببينيد",
+        EjectCarBtn = "تلپورت و سوار شدن به ماشين",
+        
+        SpecSettingTitle = "تنظیمات اعلان تماشاگر",
+        SpecHint1 = "در اين بخش بايد تگ رو تنظيم کنيد که",
+        SpecHint2 = "فقط چه اشخاصي و مقامي که اسپکت شما ميشه رو نشون بده",
+        TagAdmin = "[A] مدیر",
+        TagHelper = "[H] هلپر",
+        TagVip = "[V] بازیکن وي اي پي",
+        TagNormal = "بازيکن هاي بدون تگ مثل هاستر",
+        CloseSettingsBtn = "بستن تنظيمات",
+        
+        ObjectFinderTitle = "پيدا کردن آبجکت با آيديش",
+        ObjectHint1 = "در اين بخش با وارد کردن آيدي آبجکت",
+        ObjectHint2 = "ميتوانيد آبجکت مورد نظرتون رو پيدا کنين",
+        ObjectIDInput = "آيدي آبجکت",
+        FindObjectCheck = "پيدا کردن آبجکت"
+    }
+}
